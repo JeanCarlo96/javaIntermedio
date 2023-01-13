015e56067db367839c52cf68f1acf727eebb9657
Jean Marchesini <jeancarlo.marchesini@yahoo.es>
2023-01-12 22:04:01 -0500

    JCMA Avance en la creación de un CRUD

--- mensajes_app/src/main/java/com/repaso/mensajes_app/Inicio.java
+++ mensajes_app/src/main/java/com/repaso/mensajes_app/Inicio.java
@@ -1,21 +1,51 @@
 package com.repaso.mensajes_app;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jean Marchesini
  */
 public class Inicio {
-    
+
     public static void main(String[] args) {
-        
-        Conexion conexion = new Conexion();
-        
-        try{
-            conexion.getConnection();
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        int opcion = 0;
+
+        do {
+            System.out.println("----------------------");
+            System.out.println("Aplicación de Mensajes");
+            System.out.println("1. Crear un mensaje");
+            System.out.println("2. Listar un mensaje");
+            System.out.println("3. Eliminar un mensaje");
+            System.out.println("4. Editar un mensaje");
+            System.out.println("5. Salir");
+            
+            // Leemos la opción del usuario
+            opcion = sc.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    MensajesService.crearMensaje();
+                    break;
+                case 2:
+                    MensajesService.listarMensaje();
+                    break;
+                case 3:
+                    MensajesService.borrarMensaje();
+                    break;
+                case 4:
+                    MensajesService.editarMensaje();
+                    break;
+                default:
+                    break;
+            }
+            
+        } while (opcion != 5);
+
+
     }
-    
+
 }
---
+++ mensajes_app/src/main/java/com/repaso/mensajes_app/MensajesService.java
@@ -0,0 +1,45 @@
+package com.repaso.mensajes_app;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jean Marchesini
+ */
+public class MensajesService {
+
+    public static void crearMensaje() {
+        Scanner sc = new Scanner(System.in);
+        // Petición del nombre
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
+        // Petición del autor
+        System.out.println("Escribe tu nombre");
+        String nombre = sc.nextLine();
+
+        // Creación de objeto Mensajes
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+
+        MensajesDAO.crearMensajeDB(registro);
+    }
+
+    public static void listarMensaje() {
+        MensajesDAO.leerMensajesDB();
+    }
+
+    public static void borrarMensaje() {
+        Scanner sc = new Scanner(System.in);
+        // Solicitar el id del mensaje a borrar
+        System.out.println("Indique el ID del mensaje a borrar");
+        int id_mensaje = sc.nextInt();
+
+        // Llamar al método deborrar
+        MensajesDAO.borrarMensajeDB(id_mensaje);
+    }
+
+    public static void editarMensaje() {
+    }
+
+}
